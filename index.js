c1ecc541fd62d9e81a4a98ed121acd6749cb56a5
RonytGO <125265872+RonytGO@users.noreply.github.com>
2025-07-02 08:59:21 +0300

    Update index.js
    adding RegID or FAResponseID

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const total = req.query.total || "6500";
-  const RegID = req.query.RegID || req.query.FAResponseID || ""; // Check for both RegID and FAResponseID
+  const RegID = req.query.RegID || "";
+  const FAResponseID = req.query.FAResponseID || ""; // Check for both RegID and FAResponseID
   const paramX = "Merkaz Limud";
 
   // Build two URLs — one for success, one for failure
   const successURL = `https://puah.tfaforms.net/17` +
     `?RegID=${encodeURIComponent(RegID)}` +
-    `&FAResponseID=${encodeURIComponent(RegID)}` + // Include both parameters in the URL
+    `&FAResponseID=${encodeURIComponent(FAResponseID)}` + // Include both parameters in the URL
     `&Total=${encodeURIComponent(total)}` +
     `&ParamX=${encodeURIComponent(paramX)}` +
     `&Status=approved`;
 
   const errorURL = `https://puah.tfaforms.net/17` +
     `?RegID=${encodeURIComponent(RegID)}` +
-    `&FAResponseID=${encodeURIComponent(RegID)}` + // Include both parameters in the URL
+    `&FAResponseID=${encodeURIComponent(FAResponseID)}` + // Include both parameters in the URL
     `&Total=${encodeURIComponent(total)}` +
     `&ParamX=${encodeURIComponent(paramX)}` +
     `&Status=failed`;
 
-  console.log("Received request - total:", total, "RegID/FAResponseID:", RegID);
+  console.log("Received request - total:", total, "RegID:", RegID, "FAResponseID: ",FAResponseID);
 
   const payload = {
     terminal:    process.env.PELE_TERMINAL,
